f23201a70eee1e388c04d8492375c63589174956
junjie.zeng <junjie.zeng@focussend.com>
2019-11-01 23:09:19 +0800

    add test appi

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,4 +10,43 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express,哈哈' });
 });
 
+//注册一个路由：用户注册
+/*
+    1.path:register
+    2.请求方式：POST
+    3.接收username与password参数
+    4.admin为已注册用户
+    5.注册成功返回{code:0,data:{id:'abc',username:'',password:'123'}}
+    6.注册失败{code:1,msg:'此用户已存在'}
+ */
+/*
+    1.处理请求参数
+    2.处理
+    3.返回响应数据
+*/
+router.post('/register',function(req,res,next){//req 请求，res响应
+	//获取请求参数
+	const {username,password} = req.body;
+	//2.处理
+	if(username === 'admin'){//注册失败
+        //返回响应
+        res.send({
+        	code:1,
+        	msg:'此用户已存在'
+        })
+	}else{ //注册成功
+		//返回响应
+        res.send({
+        	code:0,
+        	data:{
+        		id:'abc',
+        		username:'',
+        		password:''
+        	}
+        })
+	}
+	
+
+})
+
 module.exports = router;
